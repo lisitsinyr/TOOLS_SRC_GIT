fd68ca1bad7015ecb28d24fb5a84f6de9cda7021
lisitsinyr <lisitsinyr@gmail.com>
2025-03-06 17:43:38 +0400

    20250306174337

--- SRC/BAT/A.WORK/lyrgit_pull.bat
+++ SRC/BAT/A.WORK/lyrgit_pull.bat
@@ -2,81 +2,531 @@
 rem -------------------------------------------------------------------
 rem lyrgit_pull.bat
 rem ----------------------------------------------------------------------------
-rem ***РџРѕР»СѓС‡РёС‚СЊ РёР·РјРµРЅРµРЅРёСЏ
+rem ***Отправить изменения
 rem ----------------------------------------------------------------------------
-rem usage: git pull [<options>] [<repository> [<refspec>...]]
+rem usage: git push [<options>] [<repository> [<refspec>...]]
 rem 
-rem     -v, --[no-]verbose    be more verbose
-rem     -q, --[no-]quiet      be more quiet
-rem     --[no-]progress       force progress reporting
-rem     --[no-]recurse-submodules[=<on-demand>]
-rem                           control for recursive fetching of submodules
-rem 
-rem Options related to merging
-rem     -r, --[no-]rebase[=(false|true|merges|interactive)]
-rem                           incorporate changes by rebasing rather than merging
-rem     -n                    do not show a diffstat at the end of the merge
-rem     --[no-]stat           show a diffstat at the end of the merge
-rem     --[no-]log[=<n>]      add (at most <n>) entries from shortlog to merge commit message
-rem     --[no-]signoff[=...]  add a Signed-off-by trailer
-rem     --[no-]squash         create a single commit instead of doing a merge
-rem     --[no-]commit         perform a commit if the merge succeeds (default)
-rem     --[no-]edit           edit message before committing
-rem     --[no-]cleanup <mode> how to strip spaces and #comments from message
-rem     --[no-]ff             allow fast-forward
-rem     --ff-only             abort if fast-forward is not possible
-rem     --[no-]verify         control use of pre-merge-commit and commit-msg hooks
-rem     --[no-]verify-signatures
-rem                           verify that the named commit has a valid GPG signature
-rem     --[no-]autostash      automatically stash/stash pop before and after
-rem     -s, --[no-]strategy <strategy>
-rem                           merge strategy to use
-rem     -X, --[no-]strategy-option <option=value>
-rem                           option for selected merge strategy
-rem     -S, --[no-]gpg-sign[=<key-id>]
-rem                           GPG sign commit
-rem     --[no-]allow-unrelated-histories
-rem                           allow merging unrelated histories
-rem 
-rem Options related to fetching
-rem     --[no-]all            fetch from all remotes
-rem     -a, --[no-]append     append to .git/FETCH_HEAD instead of overwriting
-rem     --[no-]upload-pack <path>
-rem                           path to upload pack on remote end
-rem     -f, --[no-]force      force overwrite of local branch
-rem     -t, --[no-]tags       fetch all tags and associated objects
-rem     -p, --[no-]prune      prune remote-tracking branches no longer on remote
-rem     -j, --[no-]jobs[=<n>] number of submodules pulled in parallel
-rem     --[no-]dry-run        dry run
-rem     -k, --[no-]keep       keep downloaded pack
-rem     --[no-]depth <depth>  deepen history of shallow clone
-rem     --[no-]shallow-since <time>
-rem                           deepen history of shallow repository based on time
-rem     --[no-]shallow-exclude <revision>
-rem                           deepen history of shallow clone, excluding rev
-rem     --[no-]deepen <n>     deepen history of shallow clone
-rem     --unshallow           convert to a complete repository
-rem     --[no-]update-shallow accept refs that update .git/shallow
-rem     --refmap <refmap>     specify fetch refmap
-rem     -o, --[no-]server-option <server-specific>
+rem     -v, --verbose         be more verbose
+rem     -q, --quiet           be more quiet
+rem     --repo <repository>   repository
+rem     --all                 push all refs
+rem     --mirror              mirror all refs
+rem     -d, --delete          delete refs
+rem     --tags                push tags (can't be used with --all or --mirror)
+rem     -n, --dry-run         dry run
+rem     --porcelain           machine-readable output
+rem     -f, --force           force updates
+rem     --force-with-lease[=<refname>:<expect>]
+rem                           require old value of ref to be at this value
+rem     --force-if-includes   require remote updates to be integrated locally
+rem     --recurse-submodules (check|on-demand|no)
+rem                           control recursive pushing of submodules
+rem     --thin                use thin pack
+rem     --receive-pack <receive-pack>
+rem                           receive pack program
+rem     --exec <receive-pack>
+rem                           receive pack program
+rem     -u, --set-upstream    set upstream for git pull/status
+rem     --progress            force progress reporting
+rem     --prune               prune locally removed refs
+rem     --no-verify           bypass pre-push hook
+rem     --follow-tags         push missing but relevant tags
+rem     --signed[=(yes|no|if-asked)]
+rem                           GPG sign the push
+rem     --atomic              request atomic transaction on remote side
+rem     -o, --push-option <server-specific>
 rem                           option to transmit
-rem     -4, --[no-]ipv4       use IPv4 addresses only
-rem     -6, --[no-]ipv6       use IPv6 addresses only
-rem     --[no-]negotiation-tip <revision>
-rem                           report that we have only objects reachable from this object
-rem     --[no-]show-forced-updates
-rem                           check for forced-updates on all updated branches
-rem     --[no-]set-upstream   set upstream for git pull/fetch
-rem ----------------------------------------------------------------------------
+rem     -4, --ipv4            use IPv4 addresses only
+rem     -6, --ipv6            use IPv6 addresses only
+rem -------------------------------------------------------------------
 chcp 1251>NUL
 
 setlocal enabledelayedexpansion
 
+rem --------------------------------------------------------------------------------
+rem 
+rem --------------------------------------------------------------------------------
 :begin
+    call :MAIN %* || exit /b 1
+
+    exit /b 0
+:end
+rem --------------------------------------------------------------------------------
+
+rem -----------------------------------------------
+rem procedure MAIN_INIT ()
+rem -----------------------------------------------
+:MAIN_INIT
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_INIT
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    rem -------------------------------------------------------------------
+    rem PROJECTS_LYR_ROOT - Каталог ROOT
+    rem -------------------------------------------------------------------
+    rem set PROJECTS_LYR_ROOT=D:\WORK\WIN
+    set PROJECTS_LYR_ROOT=D:
+    rem echo PROJECTS_LYR_ROOT:!PROJECTS_LYR_ROOT!
+
+    rem -------------------------------------------------------------------
+    rem PROJECTS_LYR_DIR - Каталог проектов LYR
+    rem -------------------------------------------------------------------
+    set PROJECTS_LYR_DIR=!PROJECTS_LYR_ROOT!\PROJECTS_LYR
+    rem echo PROJECTS_LYR_DIR:!PROJECTS_LYR_DIR!
+    if not exist "!PROJECTS_LYR_DIR!"\ (
+        rem echo INFO: Dir "!PROJECTS_LYR_DIR!" not exist ...
+        rem echo INFO: Create "!PROJECTS_LYR_DIR!" ...
+        rem mkdir "!PROJECTS_LYR_DIR!"
+        exit /b 1
+    )
+
+    rem -------------------------------------------------------------------
+    rem SCRIPTS_DIR - Каталог скриптов BAT
+    rem -------------------------------------------------------------------
+    if not defined SCRIPTS_DIR (
+        rem set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
+        rem set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\SCRIPT\BAT\PROJECTS_BAT\TOOLS_SRC_BAT\SRC
+        set SCRIPTS_DIR=!PROJECTS_LYR_DIR!\CHECK_LIST\SCRIPT\BAT\PROJECTS_BAT\TOOLS_SRC_BAT\SRC
+    )
+    rem echo SCRIPTS_DIR:!SCRIPTS_DIR!
+
+    rem -------------------------------------------------------------------
+    rem LIB_BAT - каталог библиотеки скриптов BAT
+    rem -------------------------------------------------------------------
+    if not defined LIB_BAT (
+        set LIB_BAT=!SCRIPTS_DIR!\LIB
+    )
+    rem echo LIB_BAT:!LIB_BAT!
+    if not exist !LIB_BAT!\ (
+        echo ERROR: Каталог библиотеки LYR !LIB_BAT! не существует...
+        exit /b 1
+    )
+
+    rem -------------------------------------------------------------------
+    rem Количество аргументов
+    rem -------------------------------------------------------------------
+    call :Read_N %* || exit /b 1
+
+    rem -------------------------------------------------------------------
+    rem Настройка среды
+    rem -------------------------------------------------------------------
+    call :SET_LIB %~f0 || exit /b 1
+
+    exit /b 0
+rem endfunction
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_SET ()
+rem --------------------------------------------------------------------------------
+:MAIN_SET
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_SET
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    exit /b 0
+rem endfunction
+
+rem --------------------------------------------------------------------------------
+rem procedure MAIN_CHECK_PARAMETR ()
+rem --------------------------------------------------------------------------------
+:MAIN_CHECK_PARAMETR
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_CHECK_PARAMETR
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    rem -------------------------------------
+    rem OPTION
+    rem -------------------------------------
+    set OPTION=
+    set O1_Name=O1
+    set O1_Caption=O1_Caption
+    set O1_Default=O1_Default
+    set O1=!O1_Default!
+    set PN_CAPTION=!O1_Caption!
+    rem call :Read_P O1 !O1! || exit /b 1
+    rem echo O1:!O1!
+    rem if defined O1 (
+    rem     set OPTION=!OPTION! -!O1_Name! "!O1!"
+    rem ) else (
+    rem     echo INFO: O1 [O1_Name:!O1_Name! O1_Caption:!O1_Caption!] not defined ...
+    rem )
+    rem echo OPTION:!OPTION!
+
+    rem -------------------------------------
+    rem ARGS
+    rem -------------------------------------
+    set ARGS=
+    set A1_Name=Comment
+    set A1_Caption=Comment
+    set A1_Default=Git Bash commit update
+    set A1_Default=%date:~6,4%%date:~3,2%%date:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+    set A1=!A1_Default!
+    set PN_CAPTION=!A1_Caption!
+    rem call :Read_P A1 !A1! || exit /b 1
+    rem echo A1:!A1!
+    rem if defined A1 (
+    rem     set ARGS=!ARGS! "!A1!"
+    rem ) else (
+    rem     echo ERROR: A1 [A1_Name:!A1_Name! A1_Caption:!A1_Caption!] not defined ... 
+    rem     set OK=
+    rem     exit /b 1
+    rem )
+    rem echo ARGS:!ARGS!
+    
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure MAIN_GIT_RUN ()
+rem =================================================
+:MAIN_GIT_RUN
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_GIT_RUN
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    rem echo ...git pull
+    call :WritePROCESS ...git pull
+    rem git pull
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure MAIN_FUNC ()
+rem =================================================
+:MAIN_FUNC
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN_FUNC
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
+    call :MAIN_GIT_RUN || exit /b 1
+
+    rem call :Pause !SLEEP! || exit /b 1
+    rem call :PressAnyKey || exit /b 1
+
+    exit /b 0
+rem endfunction
+
+rem =================================================
+rem procedure MAIN (%*)
+rem =================================================
+:MAIN
+rem beginfunction
+    set FUNCNAME=%0
+    set FUNCNAME=MAIN
+    if defined DEBUG (
+        echo DEBUG: procedure !FUNCNAME! ...
+    )
+
     set BATNAME=%~nx0
     echo Start !BATNAME! ...
 
-    echo ...git pull
-    git pull
+    set DEBUG=
 
-:Exit
+    set /a LOG_FILE_ADD=0
+
+    call :MAIN_INIT || exit /b 1
+
+    call :StartLogFile || exit /b 1
+
+    set OK=yes
+    
+    call :MAIN_SET || exit /b 1
+
+    rem if defined OK if not defined Read_N (
+    rem     call :MAIN_CHECK_PARAMETR %* || exit /b 1
+    rem )
+    call :MAIN_CHECK_PARAMETR %* || exit /b 1
+        
+    if defined OK (
+        call :MAIN_FUNC || exit /b 1
+    )
+    
+    call :StopLogFile || exit /b 1
+
+    exit /b 0
+rem endfunction
+rem =================================================
+
+rem =================================================
+rem ФУНКЦИИ LIB
+rem =================================================
+
+rem =================================================
+rem LYRConst.bat
+rem =================================================
+:LYRConst
+%LIB_BAT%\LYRConst.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRDEPLOY.bat
+rem =================================================
+:LYRDEPLOY
+%LIB_BAT%\LYRDEPLOY.bat %*
+exit /b 0
+:REPO_WORK
+%LIB_BAT%\LYRDEPLOY.bat %*
+exit /b 0
+:git_pull
+%LIB_BAT%\LYRDEPLOY.bat %*
+exit /b 0
+:DEPLOY_PROJECT
+%LIB_BAT%\LYRDEPLOY.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRLIB.bat
+rem =================================================
+:LYRLIB
+%LIB_BAT%\LYRLIB.bat %*
+exit /b 0
+:SET_LIB
+%LIB_BAT%\LYRLIB.bat %*
+exit /b 0
+:SET_POETRY
+%LIB_BAT%\LYRLIB.bat %*
+exit /b 0
+:SET_KIX
+%LIB_BAT%\LYRLIB.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRDateTime.bat
+rem =================================================
+:LYRDateTime
+%LIB_BAT%\LYRDateTime.bat %*
+exit /b 0
+:YYYYMMDDHHMMSS
+%LIB_BAT%\LYRDateTime.bat %*
+exit /b 0
+:DateTime
+%LIB_BAT%\LYRDateTime.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRFileUtils.bat
+rem =================================================
+:LYRFileUtils
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FullFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileName
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileNameWithoutExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:ExtractFileExt
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FileAttr
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:FileSize
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CreateDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CreateFile
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CheckFile
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:CurrentDir
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:COPY_FILES
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+:XCOPY_FILES
+%LIB_BAT%\LYRFileUtils.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRLog.bat
+rem =================================================
+:LYRLog
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:AddLog
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:AddLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:StartLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+:StopLogFile
+%LIB_BAT%\LYRLog.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRStrUtils.bat
+rem =================================================
+:LYRStrUtils
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:TrimLeft
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:TrimRight
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:Trim
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:Left
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:Mid
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:TrimQuotes
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+:ListToStr
+%LIB_BAT%\LYRStrUtils.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRSupport.bat
+rem =================================================
+:LYRSupport
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:PressAnyKey
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Pause
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_P
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_N
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:Read_F
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:GetDir
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:GetFile
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:FORCicle
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:GetSET
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:GetCMD
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+:CheckErrorlevel
+%LIB_BAT%\LYRSupport.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRParserINI.bat
+rem =================================================
+:LYRParserINI
+%LIB_BAT%\LYRParserINI.bat %*
+exit /b 0
+:GetINI
+%LIB_BAT%\LYRParserINI.bat %*
+exit /b 0
+:SetINI
+%LIB_BAT%\LYRParserINI.bat %*
+exit /b 0
+:GetINIParametr
+%LIB_BAT%\LYRParserINI.bat %*
+exit /b 0
+:GetFileParser
+%LIB_BAT%\LYRParserINI.bat %*
+exit /b 0
+
+rem =================================================
+rem LYRConsole.bat
+rem =================================================
+:LYRConsole
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:Write
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteCR
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteLN
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteLOG
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:SetColor
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:ReSetColor
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteNOTSET
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteDEBUG
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteINFO
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteWARNING
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteERROR
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteCRITICAL
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteBEGIN
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteEND
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WritePROCESS
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteDEBUGTEXT
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+:WriteTEXT
+%LIB_BAT%\LYRConsole.bat %*
+exit /b 0
+
+rem =================================================
